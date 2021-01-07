0827591bfacb610d5b23690fa4c4384bba1f9b49
Manikanta05 <manikantabalasubramanya47@gmail.com>
2021-01-07 11:29:12 +0530

    Added Folder

---
+++ MultipleChoiceQuiz-master/app/src/main/java/com/skooltchdev/multiplechoicequiz/QuizActivity.java
@@ -0,0 +1,154 @@
+package com.skooltchdev.multiplechoicequiz;
+
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class QuizActivity extends AppCompatActivity {
+
+    private QuestionLibrary mQuestionLibrary = new QuestionLibrary();
+
+    private TextView mScoreView;
+    private TextView mqView;
+    private TextView mQuestionView;
+    private Button mButtonChoice1;
+    private Button mButtonChoice2;
+    private Button mButtonChoice3;
+    private Button mButtonChoice4;
+
+    private String mAnswer;
+    private int mScore = 0,qeno=0;
+    private int mQuestionNumber = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+mqView=(TextView)findViewById(R.id.qno);
+        mScoreView = (TextView)findViewById(R.id.score);
+        mQuestionView = (TextView)findViewById(R.id.question);
+        mButtonChoice1 = (Button)findViewById(R.id.choice1);
+        mButtonChoice2 = (Button)findViewById(R.id.choice2);
+        mButtonChoice3 = (Button)findViewById(R.id.choice3);
+        mButtonChoice4 = (Button)findViewById(R.id.choice4);
+
+        updateQuestion();
+
+
+        //Start of Button Listener for Button1
+        mButtonChoice1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                //My logic for Button goes in here
+
+                if (mButtonChoice1.getText() == mAnswer){
+                    mScore = mScore + 1;
+                    updateScore(mScore);
+                    updateQuestion();
+                //This line of code is optiona
+                    Toast.makeText(QuizActivity.this, "correct", Toast.LENGTH_SHORT).show();
+
+                }else {
+                    Toast.makeText(QuizActivity.this, "wrong", Toast.LENGTH_SHORT).show();
+                    updateQuestion();
+                }
+            }
+        });
+
+        //End of Button Listener for Button1
+
+        //Start of Button Listener for Button2
+        mButtonChoice2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                //My logic for Button goes in here
+
+                if (mButtonChoice2.getText() == mAnswer){
+                    mScore = mScore + 1;
+                    updateScore(mScore);
+                    updateQuestion();
+                    //This line of code is optiona
+                    Toast.makeText(QuizActivity.this, "correct", Toast.LENGTH_SHORT).show();
+
+                }else {
+                    Toast.makeText(QuizActivity.this, "wrong", Toast.LENGTH_SHORT).show();
+                    updateQuestion();
+                }
+            }
+        });
+
+        //End of Button Listener for Button2
+
+
+        //Start of Button Listener for Button3
+        mButtonChoice3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                //My logic for Button goes in here
+
+                if (mButtonChoice3.getText() == mAnswer){
+                    mScore = mScore + 1;
+                    updateScore(mScore);
+                    updateQuestion();
+                    //This line of code is optiona
+                    Toast.makeText(QuizActivity.this, "correct", Toast.LENGTH_SHORT).show();
+
+                }else {
+                    Toast.makeText(QuizActivity.this, "wrong", Toast.LENGTH_SHORT).show();
+                    updateQuestion();
+                }
+            }
+        });
+
+        //End of Button Listener for Button3
+
+        //Start of Button Listener for Button3
+        mButtonChoice4.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                //My logic for Button goes in here
+
+                if (mButtonChoice4.getText() == mAnswer){
+                    mScore = mScore + 1;
+                    updateScore(mScore);
+                    updateQuestion();
+                    //This line of code is optiona
+                    Toast.makeText(QuizActivity.this, "correct", Toast.LENGTH_SHORT).show();
+
+                }else {
+                    Toast.makeText(QuizActivity.this, "wrong", Toast.LENGTH_SHORT).show();
+                    updateQuestion();
+                }
+            }
+        });
+
+    }
+
+    private void updateQuestion(){
+        mQuestionView.setText(mQuestionLibrary.getQuestion(mQuestionNumber));
+        mButtonChoice1.setText(mQuestionLibrary.getChoice1(mQuestionNumber));
+        mButtonChoice2.setText(mQuestionLibrary.getChoice2(mQuestionNumber));
+        mButtonChoice3.setText(mQuestionLibrary.getChoice3(mQuestionNumber));
+        mButtonChoice4.setText(mQuestionLibrary.getChoice4(mQuestionNumber));
+
+        mAnswer = mQuestionLibrary.getCorrectAnswer(mQuestionNumber);
+        mQuestionNumber++;
+        //for question number
+        qeno=qeno+1;
+        updateq(qeno);
+    }
+
+
+    private void updateScore(int point) {
+        mScoreView.setText("" + mScore);
+    }
+
+
+    private void updateq(int point) {
+        mqView.setText("" + qeno);
+    }
+}
